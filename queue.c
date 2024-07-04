25ad1d21f0bfeb4f4a1990fa5fe300d663981f0f
popo8712 <161846035+popo8712@users.noreply.github.com>
2024-07-05 00:54:45 +0800

    Update & insert_tail remove_tail & remove_head

--- queue.c
+++ queue.c
@@ -111,7 +111,6 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
     return remove_element;
 }
 
-
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
